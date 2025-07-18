3f8d8340394373056c77d438b62c6a09e7593603
Raja Manish <raghupatiraghav882@gmail.com>
2025-07-18 19:19:55 +0530

    exploring GET,POST,DELETE,PATCH methods

--- src/app.js
+++ src/app.js
@@ -2,15 +2,45 @@ const express = require("express");
 
 const app = express();
 
-app.use("/this", (req, res) => {
+// order of routing matters && this will match all HTTP methods
+app.use("/user", (req, res) => {
     res.set('Cache-Control', 'no-store');
-    res.send("Hi there");
+    res.send("order matters");
+});
+
+app.get("/user", (req, res) => {
+    res.set('Cache-Control', 'no-store');
+    res.send({firstname:"Raja", lastname:"Manish"});
+});
+
+app.post("/user", (req, res) => {
+    //saving to db
+    res.set('Cache-Control', 'no-store');
+    res.send("User created successfully");
+});
+
+app.delete("/user", (req, res) => {
+    res.set('Cache-Control', 'no-store');
+    res.send("User successfully deleted");
+});
+
+app.patch("/user", (req, res) => {
+    res.set('Cache-Control', 'no-store');
+    res.send("User successfully updated");
+});
+
+// order of routing matters
+app.use("/rest/1", (req, res) => {
+    res.set('Cache-Control', 'no-store');
+    res.send("Hello from Rest 1111");
 });
 
 app.use("/rest", (req, res) => {
     res.set('Cache-Control', 'no-store');
     res.send("Hello from Rest");
 });
+
+
 
 app.use("/", (req, res) => {
     res.send("Hello from Express");
