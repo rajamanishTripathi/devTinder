17c16a6b2bd1cfdcd99427a3030d90b0ec52f862
Raja Manish <raghupatiraghav882@gmail.com>
2025-07-19 20:49:34 +0530

    dynamic data for signup api

--- src/app.js
+++ src/app.js
@@ -3,21 +3,26 @@ const connectDb = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
-app.post("/signup", async(req,res) => {
-    // creating new instance of user model
-         const user = new User({
-            firstName: "hello",
-            lastName: "world",
-            emailId: "api@helo.com",
-            password: "testinghello"
-         });
 
-         try{
-            await user.save();
-            res.send("User added Successfully...");
-         }catch(err){
-            res.status(400).send("Error saving the user:" + err.message);
-         }
+// express.json() acts as middleware for the app
+// app.use will work for all the routes
+// express.json() read json object and converts it to javascript object
+// adds this javascript object back to the request(req) in the body. 
+// then req.body is javascript object and read by express
+app.use(express.json());
+
+app.post("/signup", async(req,res) => {
+
+    // console.log(req.body);
+    // creating new instance of user model
+    const user = new User(req.body);
+
+    try{
+    await user.save();
+    res.send("User added Successfully...");
+    }catch(err){
+    res.status(400).send("Error saving the user:" + err.message);
+    }
 
 });
 // start listening/making api call after connecting to database
