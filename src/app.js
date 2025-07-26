3bbd1f2153e6555fa0df40724544b126aae808f3
Raja Manish <raghupatiraghav882@gmail.com>
2025-07-26 07:49:09 +0530

    fixed individual sign,login,send request routing apis

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const connectDb = require("./config/database");
 const app = express();
-const User = require("./models/user");
-const { default: mongoose } = require("mongoose");
-
-const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
+
 
 
 // express.json() acts as middleware for the app
@@ -21,63 +17,9 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/requests");
 
-
-
-
-
-
-
-// for db operations for delete apis
-// app.delete("/user",async(req, res) => {
-//     const userId = req.body.userId;
-
-//     try{
-//        // await User.findByIdAndDelete({_id: userId});
-//         await User.findByIdAndDelete(userId);
-//         res.send("User Successfully Deleted");
-      
-//     }catch(err){
-//         res.status(400).send('Something went wrong');
-//     }
-// });
-
-// //update api
-// // any other data that are not part of db schema  is ignored by apis and not inserted 
-// app.patch("/user/:userId",async(req, res) => {
-//     const userId = req.params?.userId;
-//     const data = req.body;
- 
-//     try{
-//         const ALLOWED_UPDATE = ["about", "photourl", "gender", "age", "skills"];
-//         const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATE.includes(k));
-
-//         if(data.skills.length > 0){
-//             throw new Error ("skills cannot be more than 10");
-//         }
-
-//         if(!isUpdateAllowed){
-//             throw new Error("Update not Allowed");
-//         }
-
-//         const user =  await User.findByIdAndUpdate({_id: userId}, data,{ returnDocument:"before", runValidators:true});
-//             res.send("User Successfully Updated");   
-//             console.log(data);   
-//     }catch(err){
-//         res.status(400).send('Update failed:  ' + err.message);
-//     }
-// });
-
-
-
-// // get all users from database
-// app.get("/feed", async(req,res) =>{
-//          try {
-//             const users = await User.find({});
-//             res.send(users);
-//          } catch (error) {
-//             res.status(400).send("something went wrong");
-//          }
-// });
+app.use("/",authRouter);
+app.use("/",profileRouter);
+app.use("/",requestRouter);
 
 
 // start listening/making api call after connecting to database
