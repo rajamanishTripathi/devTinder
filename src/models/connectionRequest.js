d2d27b79878c2a6722cc5c98af4ebf9c88008ac3
Raja Manish <raghupatiraghav882@gmail.com>
2025-07-28 09:07:06 +0530

    created connection request api with compound index implemention

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -22,6 +22,18 @@ const connectionRequestSchema = new mongoose.Schema(
     { timestamps:true}
 );
 
+connectionRequestSchema.index({fromUserId:1, toUserId:1});
+
+connectionRequestSchema.pre("save",function(next) {
+    const connectionRequest = this;
+
+    // Check if the fromUserId is same as toUserId
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error("Cannot send connection request to self");
+    }
+    next();
+});
+
 const connectionRequestModel = new mongoose.model(
     "ConnectionRequest",
     connectionRequestSchema
