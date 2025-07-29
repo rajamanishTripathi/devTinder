4491a921ce0ca032581f4714b27db8b9e9a70bf8
Raja Manish <raghupatiraghav882@gmail.com>
2025-07-29 10:18:03 +0530

    creating get api for connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,8 @@ const userRouter = express.Router();
 const {userAuth} = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 
+const USER_DATA = "firstName lastName age skills gender";
+
 // Get all the pending connection request
 userRouter.get("/user/requests/received", userAuth, async(req,res) =>{
     try {
@@ -13,7 +15,7 @@ userRouter.get("/user/requests/received", userAuth, async(req,res) =>{
               toUserId: loggedInUser._id,
               status: "interested"
       //  }).populate("fromUserId",["firstName","lastName"]);
-      }).populate("fromUserId","firstName lastName age skills gender");
+      }).populate("fromUserId",USER_DATA);
       //  }).populate("fromUserId");
 
         res.json({
@@ -24,6 +26,31 @@ userRouter.get("/user/requests/received", userAuth, async(req,res) =>{
     } catch (err) {
         res.status(400).send("ERROR:  "+ err.message );
     }
+});
+
+userRouter.get("/user/connections", userAuth , async(req,res) => {
+          try{
+              const loggedInUser = req.user;
+
+              const connectionRequests = await ConnectionRequest.find({
+                $or: [
+                    {toUserId: loggedInUser._id, status: "accepted"},
+                    {fromUserId: loggedInUser._id, status: "accepted"}
+                ]
+              }).populate("fromUserId",USER_DATA).populate("toUserId",USER_DATA);
+
+              const data = connectionRequests.map((row) => {
+                if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
+                       return row.toUserId;
+                }
+                 return row.toUserId;
+              });
+
+              res.json({data});
+          }
+          catch(err){
+             res.status(400).send("ERROR:  "+ err.message );
+          }
 });
 
 module.exports = userRouter;
