4b7d34bad16f911573add6e00c190c27c4b73073
Raja Manish <raghupatiraghav882@gmail.com>
2025-07-30 09:44:59 +0530

    created feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -53,4 +53,35 @@ userRouter.get("/user/connections", userAuth , async(req,res) => {
           }
 });
 
+userRouter.get("/feed", userAuth , async(req,res) => {
+        try{
+
+            const loggedInUser = req.user;
+
+            const connectionRequests = await ConnectionRequest.find({
+                $or: [{fromUserId: loggedInUser._id},{toUserId: loggedInUser._id}]
+            }).select("fromUserId toUserId");
+
+            const hideUsersFromFeed = new Set();
+            connectionRequests.forEach((req) => {
+                hideUsersFromFeed.add(req.fromUserId.toString());
+                hideUsersFromFeed.add(req.toUserId.toString());
+            });
+
+            const users = await User.find({
+                $and:[
+                {_id:{ $nin: Array.from(hideUsersFromFeed)}},
+                {_id: { $ne: loggedInUser._id}}
+                ]
+            }).select(USER_DATA);
+
+            console.log(hideUsersFromFeed);
+
+           res.send(users);
+        }
+        catch(err){
+           res.status(400).json({message: err.message});
+        }
+});
+
 module.exports = userRouter;
