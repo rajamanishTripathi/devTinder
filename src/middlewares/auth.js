9023cae52ee465397b510f2ed28cc374972581ba
Raja Manish <raghupatiraghav882@gmail.com>
2025-08-01 19:49:54 +0530

    improved token expire response

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,7 @@ const userAuth =  async (req, res, next) => {
     try {
      const { token } = req.cookies;
      if(!token){
-        throw new Error("Invalid Token");
+        res.status(401).send("Please Login");
      }
 
      //verify this token
